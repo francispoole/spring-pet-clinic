89cfe04b7487c68a878d4cf869b7ac9197084cbe
Francis Poole <f.poole20@gmail.com>
2020-02-07 12:09:34 +0000

    #2 Adding pojo models

---
+++ src/main/java/com/francispoole/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.francispoole.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/francispoole/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.francispoole.springpetclinic.model;
+
+public class Vet extends Person {
+}
