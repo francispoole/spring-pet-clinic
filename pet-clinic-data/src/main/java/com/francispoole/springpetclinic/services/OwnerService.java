0952dd53d5922d991d4e154c189bd21b2b6b5ae7
Francis Poole <f.poole20@gmail.com>
2020-03-17 12:21:03 +0000

    Closes #5. Implement interfaces for services

---
+++ pet-clinic-data/src/main/java/com/francispoole/springpetclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package com.francispoole.springpetclinic.services;
+
+import com.francispoole.springpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
---
+++ pet-clinic-data/src/main/java/com/francispoole/springpetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package com.francispoole.springpetclinic.services;
+
+import com.francispoole.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
