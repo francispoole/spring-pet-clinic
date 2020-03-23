8d56104d25be1ff1e7ed042d2c114a0d55583338
Francis Poole <f.poole20@gmail.com>
2020-03-23 13:31:28 +0000

    Closes #15. Add vets list and update controller

--- pet-clinic-web/src/main/java/com/francispoole/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/francispoole/springpetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.francispoole.springpetclinic.controllers;
 
+import com.francispoole.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
