d31f41a724b9b035cef8e7ffb293fbaadf39fd12
Francis Poole <f.poole20@gmail.com>
2020-03-23 14:28:04 +0000

    Closes #23. Refactor to use map impl to generate id

--- pet-clinic-web/src/main/java/com/francispoole/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/francispoole/springpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Dilly");
         owner2.setLastName("Dunk");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Francis");
         vet1.setLastName("Poole");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Sam");
         vet2.setLastName("Axe");
 
