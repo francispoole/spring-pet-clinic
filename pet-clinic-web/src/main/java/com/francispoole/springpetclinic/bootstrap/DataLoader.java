dd604bc2aa587d0eae006c8ad59fdbfa440d435f
Francis Poole <f.poole20@gmail.com>
2020-03-23 13:14:36 +0000

    Closes #22. implement config

--- pet-clinic-web/src/main/java/com/francispoole/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/francispoole/springpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.francispoole.springpetclinic.model.Owner;
 import com.francispoole.springpetclinic.model.Vet;
 import com.francispoole.springpetclinic.services.OwnerService;
 import com.francispoole.springpetclinic.services.VetService;
-import com.francispoole.springpetclinic.services.map.OwnerServiceMap;
-import com.francispoole.springpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
