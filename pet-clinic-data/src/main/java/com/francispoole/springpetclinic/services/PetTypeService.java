a81b8eb7e0079da603ea36c25a66611febc480a9
Francis Poole <f.poole20@gmail.com>
2020-06-18 23:18:30 +0100

    Closes #29 - Added PetTypeService

---
+++ pet-clinic-data/src/main/java/com/francispoole/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.francispoole.springpetclinic.services;
+
+import com.francispoole.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
