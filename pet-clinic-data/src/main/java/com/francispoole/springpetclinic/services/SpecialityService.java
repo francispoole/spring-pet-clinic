0ac7758b94c0986f0c7804c7a71dde8f595de1e3
Francis Poole <f.poole20@gmail.com>
2020-06-21 22:04:18 +0100

    Closes #31 - Added SpecialityServiceMap implementation

---
+++ pet-clinic-data/src/main/java/com/francispoole/springpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.francispoole.springpetclinic.services;
+
+import com.francispoole.springpetclinic.model.Specialty;
+
+public interface SpecialityService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/francispoole/springpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package com.francispoole.springpetclinic.services.map;
+
+import com.francispoole.springpetclinic.model.Specialty;
+import com.francispoole.springpetclinic.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Specialty, Long> implements SpecialityService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
